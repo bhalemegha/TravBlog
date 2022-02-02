110337e85d674c359d728a70d487bbdd7aa42a1f
bhalemegha <megha.bhale6@gmail.com>
2022-02-02 14:43:37 -0600

    DB setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { Mongoose } from 'mongoose';
+import  mongoose  from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+const conStr = process.env.CONNECTION_STR
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(conStr)
+        .then(()=> app.listen(PORT), ()=> console.log(`server running on port: ${PORT}`))
+        .catch((err)=> console.log(err.message))
 
