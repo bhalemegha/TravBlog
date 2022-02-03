fde50c884aae392b2e42b3876911e0a455501764
bhalemegha <megha.bhale6@gmail.com>
2022-02-02 18:22:52 -0600

    Add DB connection

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import  mongoose  from 'mongoose';
 import cors from 'cors';
-
+import blogRoutes from './routes/blogs.js';
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
+app.use('/blogs', blogRoutes)
+
 const conStr = process.env.CONNECTION_STR
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 mongoose.connect(conStr)
-        .then(()=> app.listen(PORT), ()=> console.log(`server running on port: ${PORT}`))
+        .then(() => console.log("Connected to Database"))
         .catch((err)=> console.log(err.message))
 
+app.listen(PORT, ()=> console.log(`Connected on port ${PORT}`))
