455e9cd5f8e4366da31d92a710ed0fb4149bf6b0
bhalemegha <megha.bhale6@gmail.com>
2022-02-04 17:21:03 -0600

    Add blogs and form components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 // import logo from './logo.svg';
+import { Container, AppBar, Typography, Grid, Grow } from "@material-ui/core"
 import './App.css';
+import travblog from "./images/travblog.jpg"
+import Blogs from "./components/blogs/blogs";
+import Form from "./components/form";
 
 function App() {
   return (
-    <div>
-      <h1>My React App</h1>
-    </div>
+    <Container>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">TravBlog</Typography>
+          <img src={travblog} alt="travblog" height="60"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Blogs/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form></Form>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/blogs/blogs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Blog from "./blog/blog";
+const Blogs = () => {
+  return (
+    <>
+      <h1>Blogs</h1>
+      <Blog />
+      <Blog />
+      <Blog />
+    </>
+  )
+}
+
+export default Blogs;
